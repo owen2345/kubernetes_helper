0d54349140ddb4349eadec289850604f9fb5852b
owen2345 <owenperedo@gmail.com>
2022-07-12 05:31:48 -0400

    fix: deploy all defined job_apps

--- lib/templates/cd.sh
+++ lib/templates/cd.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR=`dirname "$(realpath -s "$0")"` # app_dir/.kubernetes/
 cd "$SCRIPT_DIR/../" # project directory
 
-DEPLOYMENTS="<%=[deployment.job_name, deployment.name].join(',')%>"
+DEPLOYMENTS="<%=(deployment.job_apps.map { |a| a[:name] } + [deployment.name]).join(',')%>"
 IMAGE_NAME="<%=continuous_deployment.image_name%>"
 CLUSTER_NAME="<%=continuous_deployment.cluster_name%>"
 PROJECT_NAME="<%=continuous_deployment.project_name%>"
@@ -19,11 +19,6 @@ LATEST_NAME="${IMAGE_NAME}:<%= continuous_deployment.image_tag || 'latest' %>"
 <%= include_template "_cd_google.sh" if continuous_deployment.image_name.include?('gcr.io/') %>
 <%= include_template "_cd_digital.sh" if continuous_deployment.image_name.include?('digitalocean.com/') %>
 
-## Update new secrets defined in secrets.yml as ENV vars for deployments
-<% if continuous_deployment.update_deployment %>
-  kubernetes_helper run_yml 'deployment.yml' 'kubectl apply'
-<% end %>
-
 ## Apply deployments
 IFS=',' read -r -a deployments <<< "$DEPLOYMENTS"
 for deployment in "${deployments[@]}"; do
@@ -31,3 +26,8 @@ for deployment in "${deployments[@]}"; do
 
   <%= include_template "_cd_apply_images.sh" %>
 done
+
+## Update new secrets defined in secrets.yml as ENV vars for deployments
+<% if continuous_deployment.update_deployment %>
+  kubernetes_helper run_yml 'deployment.yml' 'kubectl apply'
+<% end %>
