eb2278b8cb527e9c356f5a3df071ac355bc6bc8a
owen2345 <owenperedo@gmail.com>
2023-05-12 21:00:12 -0400

    fix: fix pushing docker image to DOcean registry

--- lib/templates/_cd_digital.sh
+++ lib/templates/_cd_digital.sh
@@ -1,15 +1,17 @@
 # Download and install doctl
 if [ -z "$(which doctl)" ]; then
   sudo snap install doctl
+  sudo snap connect doctl:dot-docker
 fi
 
 if [ ! -z "$KB_AUTH_TOKEN" ]
 then
   doctl auth init --access-token $KB_AUTH_TOKEN
+  doctl registry login --access-token $KB_AUTH_TOKEN
 fi
 
 ## Build and push containers
-echo "****** building image..."
+echo "****** building image...$DOCKER_BUILD_CMD and push via: docker push $DEPLOY_NAME"
 eval $DOCKER_BUILD_CMD
 docker push $DEPLOY_NAME
 
