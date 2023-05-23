3e75257adf21e24a08069020af484760b3651ce2
owen2345 <owenperedo@gmail.com>
2023-05-23 16:23:18 -0400

    fix: fix digital ocean CD steps feat: add support for custom ingress class

--- lib/templates/_cd_digital.sh
+++ lib/templates/_cd_digital.sh
@@ -1,20 +1,25 @@
-# Download and install doctl
 if [ -z "$(which doctl)" ]; then
-  sudo snap install doctl
-  sudo snap connect doctl:dot-docker
+  wget https://github.com/digitalocean/doctl/releases/download/v1.72.0/doctl-1.72.0-linux-amd64.tar.gz
+  tar xf ./doctl-1.72.0-linux-amd64.tar.gz
+  mv ./doctl /usr/local/bin
 fi
 
-if [ ! -z "$KB_AUTH_TOKEN" ]
-then
-  doctl auth init --access-token $KB_AUTH_TOKEN
-  doctl registry login --access-token $KB_AUTH_TOKEN
-fi
+## login doctl
+doctl auth init --access-token $KB_AUTH_TOKEN
+doctl registry login --access-token $KB_AUTH_TOKEN --expiry-seconds 1200
 
 ## Build and push containers
-echo "****** building image...$DOCKER_BUILD_CMD and push via: docker push $DEPLOY_NAME"
+echo "****** building image...$DOCKER_BUILD_CMD"
 eval $DOCKER_BUILD_CMD
+
+echo "****** pushing image...$DEPLOY_NAME"
 docker push $DEPLOY_NAME
 
-echo "****** tagging image $DEPLOY_NAME as $LATEST_NAME"
+echo "****** tagging image... $DEPLOY_NAME as $LATEST_NAME"
 docker tag $DEPLOY_NAME $LATEST_NAME
 docker push $LATEST_NAME
+
+## enable registry to update deployment
+<% if continuous_deployment.cluster_name %>
+doctl kubernetes cluster kubeconfig save --expiry-seconds 600 <%= continuous_deployment.cluster_name %>
+<% end %>
